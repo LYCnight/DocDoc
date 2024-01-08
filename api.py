852a8c7733b195bc0c13ad0849d365be69d4d88a
lycnight <lycnight@gmail.com>
2024-01-07 19:19:11 -0800

    bankend

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 #  http://127.0.0.1:8000/docs  # 文档
 
 from fastapi import FastAPI
+from util import DocGenerate 
 import uvicorn
 
 app = FastAPI()
@@ -14,6 +15,7 @@ TOKENIZER_PATH = os.environ.get("TOKENIZER_PATH", MODEL_PATH)
 
 from pydantic import BaseModel
 class Request(BaseModel):
+    knowledge: str
     prompt: str
 
 
@@ -29,6 +31,34 @@ async def chat(data: Request):
     response, history = model.chat(tokenizer, data.prompt, history=[])
     return response
 
+@app.post("/WriteContent")  # 生成目录
+async def WriteConetnt(data: Request):
+    global model, tokenizer
+    prompt = "现有信息：/n" + data.knowledge + "/n/n" + "--------------------/n"  + "请根据现有信息和你的知识，帮我生成一下这份报告的目录"
+    response, history = model.chat(tokenizer, data.prompt, history=[])
+    return response
+
+@app.post("/WriteAll")  # 生成全文
+async def WriteAll(data: Request):
+    global model, tokenizer
+    prompt = "现有目录：/n" + data.knowledge + "/n/n" + "--------------------/n" + "请根据现有目录，生成这份报告的全文"
+    ## 生成文章的方法
+    response = DocGenerate(model, prompt)  # -> response: txt
+    return response
+
+@app.post("/Continue") # 续写文本
+async def Continue(data: Request):
+    global model, tokenizer
+    prompt = "上下文：/n" + data.knowledge + "/n/n" + "--------------------/n" + "请根据上下文，续写一些内容"
+    response, history = model.chat(tokenizer, data.prompt, history=[])
+    return response
+
+@app.post("/Continue") # 优化文本
+async def Continue(data: Request):
+    global model, tokenizer
+    prompt = "上下文：/n" + data.knowledge + "/n/n" + "--------------------/n" + "请优化这段上下文的内容"
+    response, history = model.chat(tokenizer, data.prompt, history=[])
+    return response
 
 if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH, trust_remote_code=True)
---
+++ util.py
@@ -0,0 +1,5 @@
+## 文档生成的方法
+def DocGenerate(model: str, prompt: str) -> str:
+    '''
+    传入目录和模型，返回文章全文（str 格式）
+    '''
